a2135ab662ebac532bb8a179c9c52b4c701fd7c4
Baptiste LABAT <baptiste.labat@gmail.com>
2025-03-17 23:41:27 +0100

    Save work in progress on polygon with holes extrusion

--- packages/hydrostatic/src/hydrostatic/hydrostatic_3d.py
+++ packages/hydrostatic/src/hydrostatic/hydrostatic_3d.py
@@ -1,6 +1,8 @@
 import trimesh
 
 import numpy as np
+from matplotlib import pyplot as plt
+
 mesh = trimesh.Trimesh(vertices=[[0, 0, 0], [0, 0, 1], [0, 1, 0]],
                        faces=[[0, 1, 2]])
 
@@ -16,13 +18,27 @@ mesh.show()
 ext_polygon = Polygon([[0, 0], [0, 3], [3, 3], [3, 0]]) # exterior polygon
 int_polygon = Polygon([[1, 1], [2, 1], [2, 2], [1, 2]]) # interior polygon
 multi_poly = MultiPolygon([ext_polygon, int_polygon])
+
+# Plot each polygon shape directly
+for geom in multi_poly.geoms:
+    plt.plot(*geom.exterior.xy)
+
+# Set (current) axis to be equal before showing plot
+plt.gca().axis("equal")
+plt.show()
+
 vf = [trimesh.creation.triangulate_polygon(p) for p in multi_poly.geoms]
 
 vertices, f = trimesh.util.append_faces([i[0] for i in vf], [i[1] for i in vf])
 
-mesh = trimesh.Trimesh(vertices=[np.hstack((v, [0])) for v in vertices],
-                       faces=f)
+polygon = Polygon(shell=[[0, 0], [0, 3], [3, 3], [3, 0]], holes=[[[1, 1], [2, 1], [2, 2], [1, 2]]])
+vertices, faces = trimesh.creation.triangulate_polygon(polygon)
+mesh = trimesh.creation.extrude_triangulation(vertices= vertices, faces=faces, height =3)
+# mesh = trimesh.Trimesh(vertices=[np.hstack((v, [0])) for v in vertices],
+#                        faces=f)
 mesh.show()
+
+
 
 
 # mesh = trimesh.creation.extrude_polygon
